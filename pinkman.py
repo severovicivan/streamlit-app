998ddbe1058f55a84ae9a4d09c332d066814867e
Ivan Severović <44836823+severovicivan@users.noreply.github.com>
2021-01-11 11:44:30 +0100

    Final version

--- pinkman.py
+++ pinkman.py
@@ -8,6 +8,7 @@ Created on Fri Jan  8 15:18:11 2021
 import mysql.connector
 import requests
 import pickle
+import base64
 import pandas as pd
 import streamlit as st
 
@@ -65,33 +66,76 @@ def get_users():
 def build_app():
     users = pd.read_csv('Users.csv')
     transactions = pd.read_csv('Transactions.csv')
-    st.write(""" # Pinkman zadatak
-    /tIspod su prikazane tablica korisnika i tablica transakcija.
-    Tablicu korisnika sam kreirao pomoću inner joina tablica iz dump-a baze,
-    a tablicu transakcija sam kreirao dohvaćanjem vrijednosti iz API-ja.
-    """)
+    st.title('Pinkman BackEnd zadatak')
+    st.markdown("&nbsp;&nbsp;&nbsp;&nbspIspod ovog teksta su prikazane tablica korisnika i tablica transakcija.\
+    Tablicu korisnika sam kreirao pomoću inner joina tablica iz dump-a baze,\
+    a tablicu transakcija sam kreirao dohvaćanjem podataka putem API-ja.", unsafe_allow_html=True)
+    st.subheader('Korisnici')
     st.dataframe(users)
+    st.subheader('Transakcije')
     st.dataframe(transactions)
     st.write(""" 
-             /tPrethodne dvije tablice su kreirane pomoću programskog jezika
+    &nbsp;&nbsp;&nbsp;&nbspPrethodne dvije tablice su kreirane pomoću programskog jezika
     Python jer je on bolji za pripremu, čišćenje i transformaciju podataka, te
     podržava noSQL strukturu. Programski jezik PHP podržava relacijski SQL
     model baze podataka i OOP.
-            /tSljedeći korak bi bio kreiranje dva modela: Korisnik i Transakcija,
+            Sljedeći korak bi bio kreiranje dva modela: Korisnik i Transakcija,
     njihovo povezivanje 1:M vezom, te migracija i seed tablica u Laravelu.
     Tada bi se prvi zadatak mogao riješiti vrlo lako i klikom na željenog
     korisnika otvorio bi se modal sa svim podacima o njemu skupa s popisom 
     njegovih transakcija. 
-            /tS obzirom na to je u zadatku navedeno da se za njegovo rješavanje ne
+            S obzirom na to je u zadatku navedeno da se za njegovo rješavanje ne
     smije koristiti framework, a PHP znam koristiti sa uz Laravel framework, 
     vratit ću se na rješavanje zadatka putem Pythona. Kako bih dobio željene
     rezultate iz zadatka bit će potrebna još jedna denormalizacija. Spajanjem
-    tablice korisnika s tablicom transakcija dobivamo sljedeći rezultat.
+    tablice korisnika s tablicom transakcija dobivamo završni skup podataka.
     """)
     final_df = pd.merge(users, transactions, left_on='id', right_on='User_ID')
     df = final_df[['id','username','email','code','city','dob','name.1',
                   'Amount','Currency','Type','Date','Processed','Details']]
-    st.dataframe(df)
+    df.rename(columns={'name.1': 'Status', 'id': 'UID', 'dob': 'Age'}, inplace=True)
+    
+    st.subheader('Transakcije po korisniku')
+    option = st.selectbox('Odaberi korisnika', list(set(df['username'].tolist())))
+    st.dataframe(df.loc[df['username'] == option])
+    
+    st.subheader('Transakcije po tipu kartice')
+    card = st.radio("Odaberi vrstu kartice",('Visa', 'Maestro', 'MasterCard'))
+    if card == 'Visa':
+        st.dataframe(df.loc[df['Type'] == 'Visa'])
+    if card == 'Maestro':
+        st.dataframe(df.loc[df['Type'] == 'Maestro'])
+    if card == 'MasterCard':
+        st.dataframe(df.loc[df['Type'] == 'Mastercard'])
+    
+    df.loc[df['Currency'] == 'usd', 'Amount'] = df['Amount'] * 0.82
+    df['Currency'] = 'eur'
+    drzave = df.groupby(['code'])['Amount'].agg('sum')
+    # drzave['Country'] = drzave.index
+    drzave1 = pd.DataFrame({'Country': drzave.index, 'Amount': drzave.values})
+        
+    st.sidebar.subheader('Podaci za ostale programere (EUR)')
+    st.sidebar.write("&nbsp;&nbsp;&nbsp;&nbsp1USD = 0,82EUR")
+    if st.sidebar.button('Transakcije po državi'):
+        st.sidebar.dataframe(drzave)
+        csv1 = drzave1.to_csv(index=False)
+        b64 = base64.b64encode(csv1.encode()).decode()
+        link1= f'<a href="data:file/csv;base64,{b64}" download="Drzava.csv">Download csv :floppy_disk: </a>'
+        st.sidebar.markdown(link1, unsafe_allow_html=True)
+        # # st.sidebar.write('&nbsp;&nbsp;&nbsp;&nbspPodaci spremljeni :floppy_disk:')
+        
+    korisnici = df.groupby(df['username']).agg({'Amount': ['sum','count']})
+    korisnici1 = korisnici.reset_index()
+        
+    if st.sidebar.button('Transakcije po korisniku'):
+        # st.sidebar.write('&nbsp;&nbsp;&nbsp;&nbspPodaci spremljeni :floppy_disk:')
+        st.sidebar.dataframe(korisnici)
+        csv2 = korisnici1.to_csv(index=False)
+        b64 = base64.b64encode(csv2.encode()).decode()
+        link1= f'<a href="data:file/csv;base64,{b64}" download="Korisnici.csv">Download csv :floppy_disk: </a>'
+        st.sidebar.markdown(link1, unsafe_allow_html=True)
+
+    
     
 def combine():
     users = pd.read_csv('Users.csv')
@@ -99,6 +143,11 @@ def combine():
     final_df = pd.merge(users, transactions, left_on='id', right_on='User_ID')
     df = final_df[['id','username','email','code','city','dob','name.1',
                   'Amount','Currency','Type','Date','Processed','Details']]
+    # df.loc[df.Currency == 'usd', 'Amount'] = df * 1.22
+    # df.loc[df.Currency == 'usd', 'Currency'] = 'usd'
+    # for row in df.loc[df.Currency == 'usd', 'Amount']:
+    #     print(row)
+    df.loc[df['Currency'] == 'usd', 'Amount'] = df['Amount'] * 0.82
     return df
 
 if __name__ == '__main__':
